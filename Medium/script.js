cdb6fdac5f5ee5faf73371ab3343032c929d9c07
RobLazCDMX <66651813+RobLazCDMX@users.noreply.github.com>
2020-11-11 16:17:35 -0600

    Done

--- Medium/script.js
+++ Medium/script.js
@@ -1,6 +1,5 @@
 'use stricit';
 
-
 const cutPizzaSlices = function(numOfSlices){
     return function(numOfPeopleSharig){
         return `Each person gets ${ parseFloat(numOfSlices/numOfPeopleSharig).toFixed(2)} slices of pizza`;// if the input value is text type (string) use parseFloat. If the value is a number type use Math.round(num * 100) / 100 or Math.round((num + Number.EPSILON) * 100) / 100  to ensure things like 1.005 round correctly
